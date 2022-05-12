63bba60ec13e0efaa1a1cf6ddaa40a1d00fdd9aa
Fajar <fajarmh2@gmail.com>
2022-05-12 10:53:45 +0700

    Create note post

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../config/db.js');
 
-router.get("/api/v1", (req, res) => {
-    res.send("HEllo!");
+
+// create note
+router.post("/api/v1/notes", async (req, res) => {
+    try {
+        const { title, content } = req.body;
+        const newContent = await pool.query(
+            "INSERT INTO note(title, content, created_at, updated_at) VALUES ($1, $2, current_timestamp, current_timestamp) RETURNING *",
+            [title, content]);
+
+        res.json(newContent.rows[0]);
+    } catch(err) {
+        console.log(err.message);
+    }
 });
 
 module.exports = router;
